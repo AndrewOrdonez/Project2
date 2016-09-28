d8a0a476ebdb5ade024f35bdbebbd76967d35a97
AndrewOrdonez <andrewordonez5@gmail.com>
2016-09-28 11:19:15 -0600

    9/28 morning commit

--- Project2/src/fightClub/MainGame.java
+++ Project2/src/fightClub/MainGame.java
@@ -2,10 +2,19 @@ package fightClub;
 import java.util.Scanner;
 public class MainGame
 	{
-Scanner scan= new Scanner(System.in);
-		public static void main(String[] args){
-Character player = new Character();
- 
-			}
+static Scanner scan= new Scanner(System.in);
+static Character player = new Character();
+	public static void main(String[] args){
+
+ System.out.println("What would you like to be called?");
+ player.setName(scan.next());
+ System.out.println("Hello "+player.getName()+". you are an adventurer looking for treasure. You have found the entrance to a secret dungeon, type '1' if you already know the controls and would like to enter. Type anythig else if you would like to learn the controls;");
+	if (scan.nextInt()==1){
+		
+	}
+	else {
+		Tutorial.print();
+	}
+	}
 
 	}
---
+++ Project2/src/fightClub/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author Andrew.Ordonez
+ *
+ */
+package fightClub;
