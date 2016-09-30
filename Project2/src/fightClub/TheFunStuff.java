bc18755d20310f5616f37e94858b9fd216a2e3b7
AndrewOrdonez <andrewordonez5@gmail.com>
2016-09-30 02:47:15 -0600

    Update TheFunStuff.java

--- Project2/src/fightClub/TheFunStuff.java
+++ Project2/src/fightClub/TheFunStuff.java
@@ -1,6 +1,62 @@
 package fightClub;
-import java.util.ArrayList;
 public class TheFunStuff
 	{
-ArrayList<Room> dungeon;
+//Room(b north,b south,b east,b west,i stairs,i enemies,object)
+
+
+
+
+   
+
+
+
+public static Room findRoom(){
+	switch(MainGame.player.getLocation()){
+	case 1:return MainGame.r1;
+	case 2:return MainGame.r2;
+	case 3:return MainGame.r3;
+	case 4:return MainGame.r4;
+	case 5:return MainGame.r5;
+	case 6:return MainGame.r6;
+	case 7:return MainGame.r7;
+	case 8:return MainGame.r8;
+	case 9:return MainGame.r9;
+	case 10:return MainGame.r10;
+	case 11:return MainGame.r11;
+	case 12:return MainGame.r12;
+	case 13:return MainGame.r13;
+	case 14:return MainGame.r14;
+	case 15:return MainGame.r15;
+	case 16:return MainGame.r16;
+	case 17:return MainGame.r17;
+	case 18:return MainGame.r18;
+	case 19:return MainGame.r19;
+	default:return null;
 	}
+}
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
